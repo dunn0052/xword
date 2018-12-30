da6ef2bd0f02935d022e8bd7b3f653674545d982
Kevin Dunn <dunn0052@gmail.com>
2018-12-29 21:39:48 -0600

    testing ideas folder

---
+++ testing/BoardGrid.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class BoardGrid extends JPanel implements ActionListener {
+private int size = 15;
+protected JTextField[][] tiles;
+tiles = new JTextField[size][size];
+
+}
